b8797a4cdcf3fac0ae0f0fa808a67cc98739b85c
npp2107 <142470779+npp2107@users.noreply.github.com>
2025-03-26 14:18:06 +0700

    init

---
+++ IdentityServer/src/main/java/npp/socialnetwork/identityserver/IdentityServerApplication.java
@@ -0,0 +1,13 @@
+package npp.socialnetwork.identityserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityServerApplication.class, args);
+	}
+
+}
